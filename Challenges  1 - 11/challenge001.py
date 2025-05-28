c843b3d700ed9db00fb59f917277d51e1a9d4a5e
Erik Hernandez <erikcristofer02@gmail.com>
2025-05-28 15:56:02 -0400

    Confirmacion de Desafios 1-3

---
+++ Challenges  1 - 11/challenge001.py
@@ -0,0 +1,8 @@
+
+#001
+
+#Ask for the user’s first name and display the output message 
+
+name = str(input('What is your first name: '))
+
+print(f'Hello {name}')
---
+++ Challenges  1 - 11/challenge002.py
@@ -0,0 +1,7 @@
+# 002  
+# Ask for the user’s first name and then ask for  their surname and display the output message 
+
+name = str(input('What is your name?: '))
+surname = str(input('What is your surname?: '))
+
+print(f'Hello {name} {surname}')
